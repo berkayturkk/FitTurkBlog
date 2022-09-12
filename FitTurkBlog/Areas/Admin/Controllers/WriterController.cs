c05509bb3eba5e9299c74bc300c78eb44ca72518
Berkay Türk <berkayturk5481@gmail.com>
2022-09-12 11:44:55 +0300

    Admin paneli uzerinde yazar ajax ile yazar listeleme ve id'ye gore yazar getirme islemleri yapildi

---
+++ FitTurkBlog/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,51 @@
+﻿using FitTurkBlog.UI.Areas.Admin.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FitTurkBlog.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public IActionResult GetWriterById(int writerid)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.Id == writerid);
+            var jsonWriters = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriters);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                Id = 1,
+                Name = "Harun"
+            },
+            new WriterClass
+            {
+                Id = 2,
+                Name = "Bekir"
+            },
+            new WriterClass
+            {
+                Id = 3,
+                Name = "Talih"
+            }
+        };
+    }
+}
