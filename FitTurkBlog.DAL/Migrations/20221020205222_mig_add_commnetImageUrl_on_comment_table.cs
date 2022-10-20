81e4af6b2abc294045d676ee63b18c886ab4c857
Berkay Türk <berkayturk5481@gmail.com>
2022-10-21 01:17:08 +0300

    Yorum tablosuna ImageUrl property eklendi

---
+++ FitTurkBlog.DAL/Migrations/20221020205222_mig_add_commnetImageUrl_on_comment_table.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FitTurkBlog.DAL.Migrations
+{
+    public partial class mig_add_commnetImageUrl_on_comment_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CommentImageUrl",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommentImageUrl",
+                table: "Comments");
+        }
+    }
+}
