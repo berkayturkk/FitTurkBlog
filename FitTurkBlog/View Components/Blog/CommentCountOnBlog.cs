d92f270598e5da07617abc4d46182eb55630271b
Berkay Türk <berkayturk5481@gmail.com>
2022-10-21 01:35:55 +0300

    Blogların yorum sayıları blog listesinde belirtildi

---
+++ FitTurkBlog/View Components/Blog/CommentCountOnBlog.cs
@@ -0,0 +1,17 @@
+﻿using FitTurkBlog.BL.Concrete;
+using FitTurkBlog.DAL.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitTurkBlog.UI.View_Components.Blog
+{
+    public class CommentCountOnBlog : ViewComponent
+    {
+        CommentManager commentManager = new CommentManager(new EFCommentRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = commentManager.CommentGetList(id);
+            ViewBag.vCommentCount = values.Count;
+            return View(values);
+        }
+    }
+}
