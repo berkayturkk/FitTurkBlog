333838fee8bd5444e831a57285602ab8ac60b996
Berkay Türk <berkayturk5481@gmail.com>
2022-10-21 02:22:12 +0300

    Blog puanı ve yorum sayısı blog uzerinde gosterildi

--- FitTurkBlog/View Components/Blog/BlogScoreOnBlog.cs
+++ FitTurkBlog/View Components/Blog/BlogScoreOnBlog.cs
@@ -1,15 +1,19 @@
 ﻿using FitTurkBlog.BL.Concrete;
 using FitTurkBlog.DAL.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace FitTurkBlog.UI.View_Components.Blog
 {
-    public class CommentCountOnBlog : ViewComponent
+    public class BlogScoreOnBlog : ViewComponent
     {
         CommentManager commentManager = new CommentManager(new EFCommentRepository());
         public IViewComponentResult Invoke(int id)
         {
-            var values = commentManager.CommentGetList(id);
+            var values = commentManager.CommentGetList(id).Select(x => x.BlogScore).ToList();
+            var totalBlogScore = values.Sum();
+            
+            ViewBag.vTotalBlogScore = totalBlogScore;
             return View(values);
         }
     }
