1c72c5a0202de312cff906882c799cd15f3bdca1
Berkay Türk <berkayturk5481@gmail.com>
2022-10-21 17:52:17 +0300

    Footer alanında bulunan son eklenen 3 blog revize edildi

--- FitTurkBlog/View Components/Blog/BlogLast3Post.cs
+++ FitTurkBlog/View Components/Blog/BlogLast3Post.cs
@@ -2,6 +2,7 @@
 using FitTurkBlog.DAL.EntityFramework;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace FitTurkBlog.UI.View_Components.Blog
 {
@@ -13,7 +14,7 @@ namespace FitTurkBlog.UI.View_Components.Blog
         public IViewComponentResult Invoke()
         {
 
-            var values = blogManager.BlogGetLast3Blog();
+            var values = blogManager.GetBlogListWithCategoryWriter().Where(x => x.BlogStatus == true).OrderByDescending(x => x.BlogCreateDate).Take(3).ToList();
             return View(values);
         }
     }
