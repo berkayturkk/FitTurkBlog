60239b5c19e8e117b5fade2dfcb110a1e572c744
Berkay Türk <berkayturk5481@gmail.com>
2022-10-21 10:49:22 +0300

    Blog icerisinde son yazılan blog getirildi

---
+++ FitTurkBlog/View Components/Blog/LastAddedBlog.cs
@@ -0,0 +1,21 @@
+﻿using FitTurkBlog.BL.Concrete;
+using FitTurkBlog.DAL.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FitTurkBlog.UI.View_Components.Blog
+{
+    [AllowAnonymous]
+    public class LastAddedBlog : ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EFBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+
+            var values = blogManager.GetBlogListWithCategoryWriter().OrderByDescending(x => x.BlogCreateDate).Take(1).FirstOrDefault();
+            return View(values);
+        }
+    }
+}
