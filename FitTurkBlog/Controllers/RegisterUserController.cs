3c12e668639d02cea46189cb4f2eb3d39a4abae3
Berkay Türk <berkayturk5481@gmail.com>
2022-10-21 20:28:44 +0300

    Register islemi revize edildi

--- FitTurkBlog/Controllers/RegisterUserController.cs
+++ FitTurkBlog/Controllers/RegisterUserController.cs
@@ -1,8 +1,11 @@
-﻿using FitTurkBlog.Entities.Concrete;
+﻿using FitTurkBlog.BL.Concrete;
+using FitTurkBlog.DAL.EntityFramework;
+using FitTurkBlog.Entities.Concrete;
 using FitTurkBlog.UI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace FitTurkBlog.UI.Controllers
@@ -11,6 +14,8 @@ namespace FitTurkBlog.UI.Controllers
     public class RegisterUserController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+
+        Message2Manager messageManager = new Message2Manager(new EFMessage2Repository());
 
         public RegisterUserController(UserManager<AppUser> userManager)
         {
@@ -35,13 +40,29 @@ namespace FitTurkBlog.UI.Controllers
                     {
                         Email = p.Mail,
                         UserName = p.UserName,
-                        NameSurname = p.NameUsername
+                        NameSurname = p.NameUsername,
+                        ImageUrl = "/FitTurkBlog/newImages/eedebfdc-b507-4a98-b5c3-2f9b37a1a32a.jpg",
+                        Status = true
                     };
 
                     var result = await _userManager.CreateAsync(user, p.Password);
 
                     if (result.Succeeded)
                     {
+                        var sender = await _userManager.FindByNameAsync(p.UserName);
+
+                        Message2 message = new Message2
+                        {
+                            MessageSenderID = sender.Id,
+                            MessageReceiverID = 1,
+                            MessageSubject = "Yeni Kayıt",
+                            MessageDetails = "Merhaba, adım " + user.NameSurname + " FitTurkBlog sitenize yeni kayıt oldum. Herhangi bir rol atamam olmadığı için blog yazamıyorum. Rolümü yazar olarak güncellemenizi rica ediyorum. Şimdiden teşekkür ederim. İyi günler.",
+                            MessageDate = DateTime.Now,
+                            MessageStatus = true
+                        };
+
+                        messageManager.Add(message);
+
                         return RedirectToAction("Index", "LoginUser");
                     }
                     else
