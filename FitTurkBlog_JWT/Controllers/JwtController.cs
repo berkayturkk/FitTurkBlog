25663cee1f9fff6e7c5aad88da2be34c733442f4
Berkay Türk <berkayturk5481@gmail.com>
2022-10-25 18:19:26 +0300

    Projeye jwt eklendi

---
+++ FitTurkBlog_JWT/Controllers/JwtController.cs
@@ -0,0 +1,18 @@
+﻿using FitTurkBlog_JWT.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitTurkBlog_JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JwtController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+    }
+}
---
+++ FitTurkBlog_JWT/DAL/BuildToken.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace FitTurkBlog_JWT.DAL
+{
+    public class BuildToken
+    {
+        public string CreateToken()
+        {
+            var bytes = Encoding.UTF8.GetBytes("fitturkblogproject");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddHours(1), signingCredentials: credentials);
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+        }
+    }
+}
